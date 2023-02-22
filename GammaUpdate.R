589a5ceff2f257421d02e7a1bd3a138a14bc7a12
Kyle <greekdanceparty@gmail.com>
2023-02-22 16:03:18 -0700

    output now works better

--- GammaUpdate.R
+++ GammaUpdate.R
@@ -11,7 +11,7 @@ library(plotly)
 extension <- "~/Desktop/Memfault Test/BlackBox/Update1/"
 
 #file with tester's names, UUIDS etc, will use config value to determine grill size...
-testerList <- "~/Desktop/Memfault Test/BlackBox/Gamma Testers with UUIDs.xlsx"
+testerList <- "~/Desktop/Memfault Test/BlackBox/Gamma Testers with UUIDs copy.xlsx"
 testerList <- read_excel(testerList)
 
 #### Feedback forms  ####
@@ -82,12 +82,18 @@ saveOutput <- TRUE
 #result output table... How would this work with varibale number of cols?
 
 result_names <- c("UUID", "CookID", "Size", "Date", "Time", "Ignition Time", "Set Temp 1", "Time To Temp 1", "Overshoot 1", "Set Temp 2", "Time To Temp 2", "Overshoot 2", "Set Temp 3", "Time To Temp 3", "Overshoot 3", "Set Temp 4", "Time To Temp 4", "Overshoot 4", "Set Temp 5", "Time To Temp 5", "Overshoot 5", "Errors")
-results <- data.frame(matrix(nrow = 0, ncol = length(result_names)))
+results <- data.frame(matrix(nrow = 0, ncol = length(result_names)), stringsAsFactors = FALSE)
 names(results) <- result_names
 
 i <- 1
 for(i in seq(from=1, to=length(files), by=1)) {
     x <- files[i]
+    
+    # #test file...
+    # x <- "GammaUpdate1_group1_682719B2AACF_682719B2AACF1675368514_230202_1308.xlsx"
+    # 
+    # x <- paste(extension, x, sep ="")
+    #
     
     #### SETUP ####
     #decompose file name into useful information...
@@ -109,9 +115,6 @@ for(i in seq(from=1, to=length(files), by=1)) {
     tester <- testerInfo$`Gamma Tester`
     size <- if(testerInfo$Config == 2205.001) 'Large' else "Small"
   
-    #test file with errors...
-    x <- '~/Desktop/Memfault Test/BlackBox/Update1/GammaUpdate1_group1_682719B2E8A8_682719B2E8A81675205852_230131_1557.xlsx'
-    
     #### GET DATA ####
     cook_details <- read_excel(x, sheet = "Cook details")
     diag_data <- read_excel(x, sheet = "Diagnostic data")
@@ -125,14 +128,36 @@ for(i in seq(from=1, to=length(files), by=1)) {
     cook_details$grill <- na.approx(cook_details$grill, na.rm=FALSE)
     cook_details$set_temp <- na.approx(cook_details$set_temp, na.rm=FALSE)
     
-
+    #going to go up to 5 stats set temps, if they dont exist, then just need to have NA's in the rows
     # output <- c(uuid, cookid, size, date, time,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0)
-    output <- data.frame(c(uuid, cookid, size, date, time,NA,NA,NA,NA,NA,NA,NA,NA,NA,NA,NA,NA,NA,NA,NA,NA,NA))
+    output <- data.frame(matrix(nrow = 0, ncol = length(result_names)))
+    output <- data.frame(UUID = c(uuid),
+                         CookID = c(cookid),
+                         Size = c(size),
+                         Date = c(date),
+                         Time = c(time),
+                         `Ignition Time` = c(NA),
+                         `Set Temp 1` = c(NA),
+                         `Time To Temp 1` = c(NA),
+                         `Overshoot 1` = c(NA),
+                         `Set Temp 2` = c(NA),
+                         `Time To Temp 2` = c(NA),
+                         `Overshoot 2` = c(NA),
+                         `Set Temp 3` = c(NA),
+                         `Time To Temp 3` = c(NA),
+                         `Overshoot 3` = c(NA),
+                         `Set Temp 4` = c(NA),
+                         `Time To Temp 4` = c(NA),
+                         `Overshoot 4` = c(NA),
+                         `Set Temp 5` = c(NA),
+                         `Time To Temp 5` = c(NA),
+                         `Overshoot 5` = c(NA),
+                         Errors = c(NA), stringsAsFactors = FALSE)
+    
     names(output) <- result_names
     
     temps <- ncol(cook_stats)
     stat_names <- colnames(cook_stats)
-    #going to go up to 5 stats set temps, if they dont exist, then just need to have NA's in the rows
     
     #ignition time always the same...
     output[6] <- cook_stats[2,2]
@@ -144,8 +169,10 @@ for(i in seq(from=1, to=length(files), by=1)) {
   
     #TODO- test with multiple errors in a cook?
     error_index <- which(error_vals$errors != 0, arr.ind = T)
+    if(length(error_index) != 0){
+      output[22] <- error_vals$errors[error_index]
+    }
     
-    output[22] <- error_vals$errors[mat_nonzero]
     
     if(temps == 2){
       #just one set temp
@@ -154,7 +181,7 @@ for(i in seq(from=1, to=length(files), by=1)) {
       output[8] <- as.numeric(cook_stats[6,temps])
       #overshoot value
       output[9] <- as.numeric(cook_stats[1,temps])
-    } else if (temps == 5){
+    } else if (temps == 3){
       #two temps
       output[7] <- as.numeric(stat_names[temps-1])
       output[10] <- as.numeric(stat_names[temps])
@@ -193,7 +220,7 @@ for(i in seq(from=1, to=length(files), by=1)) {
       output[12] <- as.numeric(cook_stats[1,temps-2])
       output[15] <- as.numeric(cook_stats[1,temps-1])
       output[17] <- as.numeric(cook_stats[1,temps])
-    } else {
+    } else if (temps == 6){
       #five temps
       output[7] <- as.numeric(stat_names[temps-4])
       output[10] <- as.numeric(stat_names[temps-3])
@@ -216,7 +243,11 @@ for(i in seq(from=1, to=length(files), by=1)) {
     
     #### OUTPUT STATS ####
     if(saveOutput){
-      results <- rbind(results, t(output))
+      #view(output)
+      results <- rbind(results, output)
+      #view(results)
+      #idk why but the names get randomly fucked up...
+      #names(results) <- result_names
     }
     
     #### PLOT ####
@@ -225,24 +256,24 @@ for(i in seq(from=1, to=length(files), by=1)) {
     
     
     
-    grillGraph <- cook_details %>%
-      ggplot(aes(x=td)) +
-      theme_fivethirtyeight() +
-      #add labels
-      labs(title = paste(tester, "Gamma Test Cook", date) ,
-           #subtitle = "FW:01.00.09, CF:2204.a10",
-           x = "Time [sec]",
-           y = "Temp [˚F]") +
-      geom_line(aes(y=grill, color = pidout.x), size = 2, lineend = "round") +
-      geom_line(aes(y=set_temp), color = "darkred", size = 1, lineend = "round") 
-      #20 min spec line...(just was too busy with other labels for now)
-      #geom_vline(xintercept=20*60, linetype="dashed", color = "darkgreen") +
-      # geom_vline(xintercept = time_to_temp, linetype = "dashed", color = "yellowgreen") +
-      # geom_text(data = labeldf, aes(x, y, label = text), nudge_y = 15) 
-      # 
-    grillGraph 
-    
-    ggplotly(grillGraph)
-    
+    # grillGraph <- cook_details %>%
+    #   ggplot(aes(x=td)) +
+    #   theme_fivethirtyeight() +
+    #   #add labels
+    #   labs(title = paste(tester, "Gamma Test Cook", date) ,
+    #        #subtitle = "FW:01.00.09, CF:2204.a10",
+    #        x = "Time [sec]",
+    #        y = "Temp [˚F]") +
+    #   geom_line(aes(y=grill, color = pidout.x), size = 2, lineend = "round") +
+    #   geom_line(aes(y=set_temp), color = "darkred", size = 1, lineend = "round") 
+    #   #20 min spec line...(just was too busy with other labels for now)
+    #   #geom_vline(xintercept=20*60, linetype="dashed", color = "darkgreen") +
+    #   # geom_vline(xintercept = time_to_temp, linetype = "dashed", color = "yellowgreen") +
+    #   # geom_text(data = labeldf, aes(x, y, label = text), nudge_y = 15) 
+    #   # 
+    # grillGraph 
+    # 
+    # ggplotly(grillGraph)
+    # 
         
 } #end main loop
